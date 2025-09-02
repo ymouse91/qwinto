9fa5d5ecc6cba955ad33e34d2bd42301dd2dad33
ymouse91 <35890980+ymouse91@users.noreply.github.com>
2025-09-02 17:46:56 +0300

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "qwinto-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+  // lisää tähän CSS/JS jos ne on erillisissä tiedostoissa
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((resp) => resp || fetch(e.request))
+  );
+});
